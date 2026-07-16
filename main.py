daa3082a14197da6559ac9bec253df7c4e4f9dc0
Tyler Everett <treverett2@gmail.com>
2026-07-15 19:08:32 -0500

    added logger(should be in .gitignore) and game window up and running

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+
+    while running:
+        # call to logger.py to get game state logs 
+        log_state()
+
+        # processing pygame event queue
+        # quit allows to quit game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        # fill screen with black
+        # also wipes anything from last frame
+        screen.fill("black")
+
+        # flip() allows display to put game data on screen 
+        pygame.display.flip()
+
 
 
 if __name__ == "__main__":
