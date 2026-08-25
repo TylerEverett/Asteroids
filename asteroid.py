099b048aee4c7831ebcaba6ef9820644839ca23b
Tyler Everett <treverett2@gmail.com>
2026-08-25 16:58:43 -0700

    asteroid and asteroidfield spawning working

--- asteroid.py
+++ asteroid.py
@@ -3,7 +3,7 @@ import pygame
 from circleshape import CircleShape 
 from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
 
-class Asteriod(CircleShape):
+class Asteroid(CircleShape):
     def __init__(self, x: float, y: float, radius: float) -> None:
         super().__init__(x, y, radius)
 
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pygame
+
+
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
 from logger import log_state
 from player import Player
+from asteroid import Asteroid 
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -11,10 +16,19 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH - 1, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
+
+
     dt = 0.0
     
     while True:
