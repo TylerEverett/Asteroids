8876aab787c1afeb8939c243d2849bf5a09004b5
Tyler Everett <treverett2@gmail.com>
2026-08-27 20:21:17 -0700

    add splitting to asteriod class

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,9 @@
 import pygame
+import random
 
-from circleshape import CircleShape 
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from circleshape import CircleShape
+from logger log_event
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
     def __init__(self, x: float, y: float, radius: float) -> None:
@@ -15,3 +17,22 @@ class Asteroid(CircleShape):
         # Override the update() method so that it moves in a straight line at constant speed. 
         # On each frame, it should add (self.velocity * dt) to its position (get self.velocity from its parent class, CircleShape).
         self.position += self.velocity * dt
+
+    def split(self) -> None:
+        self.kill()
+        
+        if(self.radius <= ASTEROID_MIN_RADIUS):
+            return
+        
+        log_event("asteroid_split")
+        
+        angle = random.uniform(20, 50)
+        pos_velocity = self.velocity.rotate(angle)
+        neg_velocity = self.velocity.rotate(-angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        pos_asteroid = Asteroid(self.x, self.y, new_radius)
+        asteroid.velocity = pos_velocity * 1.2
+        neg_asteroid = Asteroid(self.x, self.y, new_radius)
+        asteroid.velocity = neg_velocity * 1.2
